7bc4edc41ab65ce4bb530f6c6a304fe97363d52e
jadeilsonm <75817805+jadeilsonm@users.noreply.github.com>
2021-06-18 18:45:12 -0300

    Add variaveis de senha e nome de personagem.

--- Desafio.py
+++ Desafio.py
@@ -2,3 +2,9 @@ while True:
     nome = input("nome: ").strip()
     if nome in (""," "):
         break
+    else:
+        nome_personagem = input("nome do seu personagem: ").strip()
+        senha = input("senha:").strip()
+        confimar_Senha = input("Confirmar senha:")
+        if senha == confimar_Senha:
+            print('')
