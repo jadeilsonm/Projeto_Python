fc417a785b1cc0b4420b065f4975f5bc21cfddfd
jadeilsonm <75817805+jadeilsonm@users.noreply.github.com>
2021-07-01 16:05:46 -0300

    Update Function

--- Desafio.py
+++ Desafio.py
@@ -1,10 +1,15 @@
-while True:
-    nome = input("nome: ").strip()
-    if nome in (""," "):
-        break
-    else:
-        nome_personagem = input("nome do seu personagem: ").strip()
-        senha = input("senha:").strip()
-        confimar_Senha = input("Confirmar senha:")
-        if senha == confimar_Senha:
-            print('')
+import Function
+import random
+num_jogadores = int(input('Quantos jogadores ira participar? '))
+if num_jogadores >= 1 and num_jogadores <= 4:
+    jogadores = Function.jogador(num_jogadores)
+    print('''
+    Este desafio tem o intuito de demonstrar como funciona um jogo no qual tem como princípio, 
+    fazer com que os jogadores trabalhem a matemática, a história, raciocínio lógico e conhecimentos
+     gerais ao tentar acertar um número gerado de forma aleatória
+    ''')
+    secreto = random.randrange(1,1000)
+    dicas = Function.dica(secreto)
+    Function.rodadas(jogadores, secreto,dicas)
+else:
+    print('Não permitimos essa quantidade de Participante!')
--- Function.py
+++ Function.py
@@ -1,32 +1,69 @@
+import random
+def jogador(num_jogadores):
+    jogadores = []
+    for i in range(num_jogadores):
+        nome = input('Nome do jogador %i' % (i+1))
+        jogadores.append(nome)
+    return jogadores
+
+def rodadas(jogadores,secreto,dicas):
+    palpites = []
+    for x in range(5):
+        dica = random.choice(dicas)
+        dicas.pop(dicas.index(dica))
+        if x >= 1:
+            print(dica)
+        for jogador in jogadores:
+            print(jogador)
+            palpite = int(input('Qual o seu palpite: '))
+            x = [jogador, palpite]
+            palpites.append(x)
+        for palpite in palpites:
+            if palpite[1] == secreto:
+                break
+
 def dica(secreto):
-    dicas = ''
-    lista = ['O número é impar','O número é divisível por 22','O número está num intervalo de 1 e 1432','A raiz desse número é maior que 32','O número é menor do que a idade do descobrimento do Brasil','O número é maior do que a quantidade de estados brasileiros']
-    if secreto % 2 != 0 and dicas == '':
-        if 'O número é impar' in lista:
-            dicas = 'O número é impar'
-            lista.pop(lista.index('O número é impar'))
+    dicas = []
 
-    if secreto % 22 == 0 and dicas == '':
-        if 'O número é divisível por 22' in lista:
-            dicas = 'O número é divisível por 22'
-            lista.pop(lista.index('O número é divisível por 22'))
+    if secreto % 2 != 0:
+        if 'O número é impar' not in dicas:
+            dicas.append('O número é impar')
+    else:
+        if 'O número é par' not in dicas:
+            dicas.append('O número é par')
 
-    if secreto >= 1 and secreto < 1432 and dicas == '':
-        if 'O número está num intervalo de 1 e 1432' in lista:
-            dicas = "O número está num intervalo de 1 e 1432"
-            lista.pop(lista.index('O número está num intervalo de 1 e 1432'))
 
-    if secreto ** 1/2 > 32 and dicas == '':
-        if 'A raiz desse número é maior que 32' in lista:
-            dicas = 'A raiz desse número é maior que 32'
-            lista.pop(lista.index('A raiz desse número é maior que 32'))
+    if secreto % 22 == 0:
+        if 'O número é divisível por 22' not in dicas:
+            dicas.append('O número é divisível por 22')
 
-    elif secreto < 520 and dicas == '':
-        if 'O número é menor do que a idade do descobrimento do Brasil' in lista:
-            dicas = 'O número é menor do que a idade do descobrimento do Brasil'
-            lista.pop(lista.index('O número é menor do que a idade do descobrimento do Brasil'))
 
-    elif secreto > 24 and dicas == '':
-        if 'O número é maior do que a quantidade de estados brasileiros' in lista:
-            dicas = 'O número é maior do que a quantidade de estados brasileiros'
-            lista.pop(lista.index('O número é maior do que a quantidade de estados brasileiros'))
+    if secreto >= 1 and secreto < 1432:
+        if 'O número está num intervalo de 1 e 1432' not in dicas:
+            dicas.append("O número está num intervalo de 1 e 1432")
+
+
+    if secreto ** 1/2 > 32:
+        if 'A raiz desse número é maior que 32' not in dicas:
+            dicas.append('A raiz desse número é maior que 32')
+    else:
+        if 'A raiz desse número é menor que 32' not in dicas:
+            dicas.append('A raiz desse número é menor que 32')
+
+
+    if secreto < 520:
+        if 'O número é menor do que a idade do descobrimento do Brasil' not in dicas:
+            dicas.append('O número é menor do que a idade do descobrimento do Brasil')
+    else:
+        if 'O número é maior do que a idade do descobrimento do Brasil' not in dicas:
+            dicas.append('O número é maior do que a idade do descobrimento do Brasil')
+
+
+    if secreto > 24 :
+        if 'O número é maior do que a quantidade de estados brasileiros' not in dicas:
+            dicas.append('O número é maior do que a quantidade de estados brasileiros')
+    else:
+        if 'O número é menor do que a quantidade de estados brasileiros' not in dicas:
+            dicas.append('O número é menor do que a quantidade de estados brasileiros')
+
+    return dicas
